5a6788524dd8f3df7467effabd3c6eb9e6e07138
Daniel Ribeiro da Costa <ribeirodacostadaniel57@gmail.com>
2025-04-26 17:36:23 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import style from './App.module.css'
 
 import {Menu} from './components/menu.jsx'
 
+import luta from "./assets/images/luta.mp4";
+
 function App() {
   const [aparecerVideo, setAparecerVideo] = useState(false)
   const [aparecerBotao, setAparecerBotao] = useState(false)
@@ -15,24 +17,29 @@ function App() {
      {/* {first ? <nav>batata</nav>: <nav>Arroz</nav>}
      <button onClick={() => setfirst(true)}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/640px-Hamburger_icon.svg.png" alt="" /></button> */}
    <Menu option01='Início' option02='Personagens'/>
+ 
    <section className={style.Inicio}>
+    <video autoPlay muted loop className={style.backgroundVideo}>
+              <source src={luta}  type="video/mp4"/>
+      </video>
       <div className={style.primeira}>
         <img src={logo} alt="logo" className={style.logo}/>
        
         <div className={style.wrapInfo}>
-        <p className={style.paragrafoLP}>Dragon Ball é um mangá criado por Akira Toriyama, lançado em novembro de 1984 na revista Weekly Shonen Jump. Devido ao enorme sucesso, a obra ganhou uma adaptação 
-          para anime em fevereiro de 1986, produzida pela Toei Animation. Posteriormente, em abril de 1989, o estúdio também adaptou a segunda parte do mangá. Anos depois, em junho de 2015, foi lançada uma nova 
-          animação original intitulada Dragon Ball Super. A história acompanha a vida de Son Goku e suas aventuras ao lado de seus amigos.</p>
-        <button className={style.btAparecerB} onClick={ () => {setAparecerBotao(true)}}>Quero ver o filme!</button>
+        <p className={style.paragrafoLP}>Durante o Torneio do Poder em "Dragon Ball Super", Goku enfrenta Jiren, o guerreiro mais forte 
+          do Universo 11. Em uma luta intensa e cheia de reviravoltas, Goku ultrapassa seus limites ao despertar o Instinto Superior, uma técnica 
+          que permite movimentos quase automáticos. Mesmo com o imenso poder de Jiren, Goku mostra seu espírito de luta incansável, resultando em uma das batalhas mais emocionantes de toda a franquia.</p>
+        <button className={style.btAparecerB} onClick={ () => {setAparecerBotao(true)}}>Quero ver a luta!</button>
        
         </div>
         </div>
        
        <div className={style.AparecerVideo}>
        {aparecerBotao &&   <button className={style.btAparecerV} onClick={() => {setAparecerVideo(true); } }>Tenho certeza de quero ver!</button>}
-       {aparecerVideo && <iframe width="90%" height="200" src="https://www.youtube.com/embed/bPXQJF9WIR8?si=uyLZxm0_2dpLyGqe&amp;start=3" 
-       title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-       referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>}
+      
+       {aparecerVideo && <iframe width="90%" height="200" src="https://www.youtube.com/embed/mFoOlpKQiMk?si=9qOW0fnxcX56vi06" 
+       title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+       picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>}
        </div>
       
   
--- src/Req.jsx
+++ src/Req.jsx
@@ -32,6 +32,7 @@ export default function Req() {
       <Menu option01="Voltar" option02="" />
 
       <section className={style.wrapPage}>
+      
         <div className={style.imageApi}>
           <img src={logoApi} alt="logoApi" className={style.logoApi} />
         </div>
@@ -39,7 +40,7 @@ export default function Req() {
         <div className={style.containerInput}>
           <input
             type="text"
-            placeholder="Digite uma pagina de 1 a 5"
+            placeholder="Digite uma pagina de 1 a 6"
             value={page}
             onChange={(e) => setPage(e.target.value)}
           />
